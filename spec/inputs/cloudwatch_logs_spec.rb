128e348711008155c3b42733b4a21f8d938e0639
Luke Waite <lwaite@gmail.com>
2017-06-28 18:19:59 -0400

    Test find_log_groups when log_group_prefix is set to false

--- spec/inputs/cloudwatch_logs_spec.rb
+++ spec/inputs/cloudwatch_logs_spec.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
-require "logstash/devutils/rspec/spec_helper"
-require "logstash/inputs/cloudwatch_logs"
+require 'logstash/devutils/rspec/spec_helper'
+require 'logstash/inputs/cloudwatch_logs'
+require 'aws-sdk-resources'
+require 'aws-sdk'
 
 describe LogStash::Inputs::CloudWatch_Logs do
   let(:config) {
@@ -18,21 +20,51 @@ describe LogStash::Inputs::CloudWatch_Logs do
   end
 
 
-  context "when interrupting the plugin" do
-    let(:config) { super.merge({ "interval" => 5 }) }
+  context 'when interrupting the plugin' do
+    let(:config) {super.merge({'interval' => 5})}
 
     before do
-      expect_any_instance_of(LogStash::Inputs::CloudWatch_Logs).to receive(:process_group)
+      expect_any_instance_of(LogStash::Inputs::CloudWatch_Logs).to receive(:process_group).and_return(nil)
     end
 
-    it_behaves_like "an interruptible input plugin"
+    it_behaves_like 'an interruptible input plugin'
   end
 
   describe '#register' do
     subject {LogStash::Inputs::CloudWatch_Logs.new(config)}
 
-    it "registers succesfully" do
+    it 'registers succesfully' do
       expect {subject.register}.to_not raise_error
     end
   end
+
+  describe '#find_log_groups without prefix true' do
+    context 'with an array in the config' do
+      subject {LogStash::Inputs::CloudWatch_Logs.new(config)}
+
+      it 'passes through configuration' do
+        expect(subject.find_log_groups).to eq(['sample-log-group'])
+      end
+    end
+
+    context 'with a single string in the log_group' do
+      subject {LogStash::Inputs::CloudWatch_Logs.new(config.merge({'log_group' => 'sample-log-group-string'}))}
+
+      it 'array-ifies the single string' do
+        expect(subject.find_log_groups).to eq(['sample-log-group-string'])
+      end
+    end
+  end
+
+  # describe '#find_log_groups with prefix true' do
+  #   subject {LogStash::Inputs::CloudWatch_Logs.new(config.merge({'log_group_prefix' => true}))}
+  #
+  #   before(:each) {subject.register}
+  #
+  #   it 'should create list of prefixes' do
+  #     expect_any_instance_of(Aws::CloudWatchLogs::Resource).to receive(:describe_log_groups).and_return({'log_groups' => [{'log_group_name' => '1'},{'log_group_name' => '2'}]})
+  #     expect(subject.find_log_groups).to eq(['sample-log-group-1', 'sample-log-group-2'])
+  #   end
+  # end
+
 end
